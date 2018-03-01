4e8a47466e5c9270a8ff04e23b33ad75622ad53c
Martin Escoto <escotom@oregonstate.edu>
2018-03-01 08:26:38 -0800

    Add project files.

---
+++ Tutorials_With_Bucky/Tutorials_With_Bucky.py
@@ -0,0 +1,11 @@
+foods = ['bacon', 'tuna', 'ham', 'snausages', 'beef']
+
+for f in foods:
+    print(f)
+    print(len(f))
+
+name = ['martin', 'helen', 'jack', 'ruth', 'peter', 'sam']
+
+for n in name:
+    print(n)
+    print(len(n))
