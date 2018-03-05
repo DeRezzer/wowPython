4c19ca1fc12a13508c0621200673bdcb8a18fbd0
Martin Escoto <escotom@oregonstate.edu>
2018-03-04 21:09:16 -0800

    learned how to use break and comments in python, plus learned how range a little differently

--- Tutorials_With_Bucky/Tutorials_With_Bucky.py
+++ Tutorials_With_Bucky/Tutorials_With_Bucky.py
@@ -1,7 +1,13 @@
-for x in range(10, 40, 5):
-    print(x)
+magicNumber = 26
 
-butter = 5
-while butter < 10:
-    print(butter)
-    butter += 1
+
+for n in range(101):
+    if n is magicNumber:
+        print(n,"Is the magic number")
+        break #learned how to use the break statement in python
+    else:
+        print(n)
+
+for f in range (101):
+    if f % 4 is 0:
+        print(f) #simple code that prints out multiples of 4 between 0 and 100 coooooolio  
